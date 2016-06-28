adb982fb92d88aa023e3ddd451432f67b82e2094
Yashu Seth <yashuseth2503@gmail.com>
2016-06-29 03:24:50 +0530

    course 1 week 3 assignment

---
+++ Functional Programming Principles in Scala/week3/objsets/src/test/scala/objsets/TweetSetSuite.scala
@@ -0,0 +1,98 @@
+package objsets
+
+import org.scalatest.FunSuite
+
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TweetSetSuite extends FunSuite {
+  trait TestSets {
+    val set1 = new Empty
+    val set2 = set1.incl(new Tweet("a", "a body", 20))
+    val set3 = set2.incl(new Tweet("b", "b body", 20))
+    val c = new Tweet("c", "c body", 7)
+    val d = new Tweet("d", "d body", 9)
+    val set4c = set3.incl(c)
+    val set4d = set3.incl(d)
+    val set5 = set4c.incl(d)
+    val set6 = set5 incl new Tweet("yashu", "nothing to say", 25)
+    val all_tweets = TweetReader.allTweets
+  }
+
+  def asSet(tweets: TweetSet): Set[Tweet] = {
+    var res = Set[Tweet]()
+    tweets.foreach(res += _)
+    res
+  }
+
+  def size(set: TweetSet): Int = asSet(set).size
+
+  test("equlaity of tweets") {
+    new TestSets {
+      val tw = new Tweet("yashu", "no post", 1)
+      val tw2 = new Tweet("yashu2", "no post", 12)
+      assert(tw != tw2)
+    }
+  }
+
+  test("filter: on empty set") {
+    new TestSets {
+      assert(size(set1.filter(tw => tw.user == "a")) === 0)
+    }
+  }
+
+  test("filter: a on set5") {
+    new TestSets {
+      assert(size(set5.filter(tw => tw.user == "a")) === 1)
+    }
+  }
+
+  test("filter: 20 on set5") {
+    new TestSets {
+      assert(size(set5.filter(tw => tw.retweets == 20)) === 2)
+    }
+  }
+
+  test("union: set4c and set4d") {
+    new TestSets {
+      assert(size(set4c.union(set4d)) === 4)
+    }
+  }
+
+  test("union: with empty set (1)") {
+    new TestSets {
+      assert(size(set5.union(set1)) === 4)
+    }
+  }
+
+  test("union: with empty set (2)") {
+    new TestSets {
+      assert(size(set1.union(set5)) === 4)
+    }
+  }
+
+  test("most retweeted tweet") {
+    new TestSets {
+      val max_tw = all_tweets.mostRetweeted
+      assert(new Tweet("yashu", "nothing to say", 25) != max_tw)
+    }
+  }
+
+  test("descending: set6") {
+    new TestSets {
+      val trends = set6.descendingByRetweet
+      assert(!trends.isEmpty)
+      assert(trends.head.user == "yashu")
+
+      val fake_tw = new Tweet("yashu", "I lost my android phone.", 5)
+      val iphone_tw = new Tweet("raone", "ios rocks. Bazingaa!! galaxy", 21)
+      val fake_tws = new Empty incl fake_tw incl new Tweet("yashu", "Nexus on google??", 3)
+      val tws = GoogleVsApple.filter_from_list(GoogleVsApple.google, fake_tws)
+      assert(tws.mostRetweeted.user === fake_tw.user)
+      assert(tws.mostRetweeted.text === fake_tw.text)
+    }
+  }
+
+  }
